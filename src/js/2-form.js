793f77e97360d1e357cdcafed04529e6fe82263e
gmeng12 <6080043@gmail.com>
2024-02-17 13:55:46 +0100

    Js-hw-09

---
+++ src/js/2-form.js
@@ -0,0 +1,37 @@
+const STORAGE_KEY = "feedback-msg";
+
+const form = document.querySelector(".feedback-form");
+
+
+function readFormData(form) {
+    const message = form.message.value;
+    const email = form.email.value;
+    return {
+        message,
+        email
+    }
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    localStorage.removeItem(STORAGE_KEY);
+    form.reset();
+})
+
+form.addEventListener('input', (event) => {
+    event.preventDefault();
+    const data = readFormData(event.currentTarget);
+    const jsonData = JSON.stringify(data);
+    localStorage.setItem(STORAGE_KEY, jsonData);
+})
+
+const rawData = localStorage.getItem(STORAGE_KEY);
+if (rawData) {
+    const data = JSON.parse(rawData);
+    if (data.email !== undefined) {
+        form.email.value = data.email;
+    }
+    if (data.message !== undefined) {
+        form.message.value = data.message;
+    }
+}
